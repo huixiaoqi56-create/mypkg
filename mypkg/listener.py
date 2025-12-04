20986f3f925644677c7c4d2d4b85173e53f74671
sinosaki <huixiaoqi56@gmail.com>
2025-12-04 22:28:14 +0900

    8

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,31 +1,16 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.srv import Query
-
+from std_msgs.msg import Int16
 rclpy.init()
-node = Node( "listener")
-#def cb(msg):
- #   global node
-  #  node.get_logger().info("Listen: %d" % msg.data)
+
+
+node = Node("listener")
+
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
 
 def main():
-   # pub = node.create_subscription(Person, "person", cb, 10)
-    #rclpy.spin(node)
-   client = node.create_client(Query,'query')
-   while not client.wait_for_service(timeout_sec=1.0):
-       node.get_logger().info('待機中')
-   req = Query.Request()
-   req.name = "hakozaki"
-   future = client .call_async(req)
-   while rclpy.ok():
-       rclpy.spin_once(node)
-       if future.done():
-          try:
-              responce = future.result()
-          except:
-              node.get_logger().info('呼び出し失敗')
-          else:
-              node.get_logger().info("agt: {}" .format(response.age))
-          break
-   node.destroy_node()
-   rclpy.shutdown()
+    pub = node.create_subscription(Int16, "countup", cb, 10)
+    rclpy.spin(node)
+
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,26 +1,24 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query                                       #from std_msgs.msg import Person
+from std_msgs.msg import Int16
 
 rclpy.init()
-node = Node( "talker")
-#pub = node.create_publisher(Int16, "person", 10)
-#n=0
-def cb(request,response):
-    #global n
-    #msg = Person()
-    #msg.name = "hakozaki":
-    if request.name == "hakozaki":
-       response.age == 46
-    else:
-        response.age =255
-    return response
 
-#    msg.age = n
- #   pub.publish(msg)
-  #  n += 1
+node = Node("talker")
+
+pub = node.create_publisher(Int16, "countup", 10)
+
+n = 0
+
+
+def cb():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
+    n += 1
+
 
 def main():
-   # node.create_timer(0.5, cb)
-    srv = node.create_service(Query, "query",cb)
+    node.create_timer(0.5, cb)
     rclpy.spin(node)
